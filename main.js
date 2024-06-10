08c25598670ecfc4f838f840e864aafa03ad82c8
Shumimran <164326671+Shumimran@users.noreply.github.com>
2024-06-10 13:35:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+// Assigning and Updating Variables: Create a function where you declare a variable using 
+// let and assign an initial value. Then, update its value within the same function and 
+// log both the initial and updated values.
+function updateVariable() {
+    let number = 10;
+    console.log("initial value:", number);
+    number = 20;
+    console.log("update value:", number);
+}
+updateVariable();
